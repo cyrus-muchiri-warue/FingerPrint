d1d7bf14ef99a77a26fd23d3ef78a6b20add02b4
cyrusmuchiriwarue <cyrus5078@gmail.com>
2020-10-31 15:34:05 +0300

    first commit

---
+++ app/src/main/java/ke/co/nevon/fingerprint/MainActivity.java
@@ -0,0 +1,19 @@
+package ke.co.nevon.fingerprint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.label);
+    }
+}
